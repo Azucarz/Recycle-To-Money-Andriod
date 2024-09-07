d39d17713b9aa994612de6cbea91d525448506a7
KoDraaa25 <157332680+KoDraaa25@users.noreply.github.com>
2024-09-07 18:03:06 +0900

    database response
    sudah sampai dataa base tapi belum respon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:smart_waste/login_page.dart';
+import 'login_page.dart';
 
 Future<void> main() async {
   runApp(const MainApp());
@@ -11,7 +11,6 @@ Future<void> main() async {
     url: 'https://eicuolgrlmpkywitfblr.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpY3VvbGdybG1wa3l3aXRmYmxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3NzM0OTYsImV4cCI6MjA0MDM0OTQ5Nn0.e_fxtoz74yBNO1ircGFci2lIQ6dGeTNL02oTJhggx7U',
   );
-
 }
 
 class MainApp extends StatelessWidget {
